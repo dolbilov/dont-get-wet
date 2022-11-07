8e3afb5dafc34877147a194c088dd5706859091c
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2022-11-07 15:41:37 +0300

    feat: add call of function to get and display forecast

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import './pages/index.scss';
 import { Coords } from "./components/coords";
 import { Weather } from "./components/weather";
 
-const forecastPeriodInDays = 3;
 
 const currentTemperatureText = document.querySelector(".card__temperature-value");
-const temperatureTexts = document.querySelectorAll(".card__temperature-value.card__temperature-value_size_small");
 const currentWeatherDescription = document.querySelector(".card__caption");
-const dateTexts = document.querySelectorAll(".card__date.card__date_weight_light");
-const markupElements = {}
+const temperatureTexts = document.querySelectorAll(".card__temperature-value.card__temperature-value_size_small");
+const dataTexts = document.querySelectorAll(".card__date.card__date_weight_light");
+const markupElements = {
+  currentTemperatureText: currentTemperatureText,
+  currentWeatherDescription: currentWeatherDescription,
+  temperatureTexts: temperatureTexts,
+  dataTexts: dataTexts
+}
 
 const coordsObject = new Coords();
 coordsObject.getUserCoords();
 const userCoords = coordsObject.coords;
 
 const weatherObject = new Weather(userCoords);
-weatherObject.getWeather();
-weatherObject.showWeather(markupElements);
+weatherObject.getAndShowWeather(markupElements);
