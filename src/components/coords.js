92afae4715fb1591a0c7a489b3283f4571e3a945
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2022-11-07 14:46:47 +0300

    feat: finish `coords` class

--- src/components/coords.js
+++ src/components/coords.js
@@ -1,28 +1,17 @@
-import {getWeatherData, handleError} from "./api";
-
-const handleCoordsSuccess = (pos) => {
-  const crd = pos.coords;
-
-  // TODO: update this
-  getWeatherData(crd.latitude, crd.longitude)
-    .then(data => {
-      console.log(data);
-      const currentWeather = data.current_weather;
-      const temperature = currentWeather.temperature;
-    })
-    .catch(handleError);
-
-}
-
-const handleCoordsError = (err) =>
-  console.warn(`ERROR ${err.code}: ${err.message}`)
-
-
-export const getUserCoords = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(handleCoordsSuccess, handleCoordsError);
+class Coords {
+  constructor() {
+    this.coords = { latitude: 55.7558, longitude: 37.6176 }
   }
-  else {
-    console.warn(`Geolocation doesn't support`);
+
+  _handleSuccess = (pos) => this.coords =  pos.coords;
+  _handleCoordsError = (err) => console.warn(`ERROR ${err.code}: ${err.message}`);
+
+  getUserCoords = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._handleSuccess, this._handleCoordsError);
+    }
+    else {
+      console.warn(`Geolocation doesn't support`);
+    }
   }
 }
