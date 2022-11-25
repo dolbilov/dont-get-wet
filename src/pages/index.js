62c922d7a7c764ccb5f33a0f367edb171e00ac2d
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2022-11-25 09:02:36 +0300

    fix: fix FOUC

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import Api from "../components/Api";
 import DropdownList from "../components/DropdownList.js";
 import { markupElements } from "../utils/constants";
 
+
 const dropdownList = new DropdownList("dropdown", constants.cityCoords, updateForecast);
 const coordsObject = new Coords(updateForecast, markupElements.currentCity);
 const api = new Api();
@@ -42,6 +43,7 @@ const updateForecast = (coords, cityName) => {
       data.current_city = cityName;
       const weather = new Weather(data);
       weather.showWeather(constants.markupElements);
+      constants.mainContainer.style.visibility = "visible";
     })
     .catch(api.handleError);
 }
